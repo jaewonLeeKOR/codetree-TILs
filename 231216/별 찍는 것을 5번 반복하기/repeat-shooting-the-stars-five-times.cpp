e0dd8a80f9fa12a0a09fdb7d85646ca7870710ad
이재원 <58386334+jaewonLeeKOR@users.noreply.github.com>
2023-12-16 23:49:00 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231216/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    for(int i=0; i<5; i++) {
+        for(int j=0; j<10; j++)
+            std::cout << '*';
+        std::cout << '\n';
+    }
+    return 0;
+}
