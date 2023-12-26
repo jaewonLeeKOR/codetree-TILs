1f717749f07abd831c9203d8a842dfbfa0fa4009
이재원 <58386334+jaewonLeeKOR@users.noreply.github.com>
2023-12-26 16:40:09 +0900

    Title: 함수를 이용한 369 게임, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231226/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int cnt = 0, x, y;
+    cin >> x >> y;
+    for(int i=x; i<=y; i++) {
+        if(i%3 == 0) {
+            cnt++;
+            continue;
+        }
+        int num = i;
+        while(num) {
+            if((num % 10)%3 == 0) {
+                cnt++;
+                break;
+            }
+            num = num / 10;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
